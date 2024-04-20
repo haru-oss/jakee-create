6017eb023fef5c50aa56111cd027f6ac6f49846d
haru-oss <hauyaishungry@gmail.com>
2024-04-20 15:07:57 +0900

    gitでミスった為ファイルをコピーしてやり直し。これから削除機能を書く

---
+++ my-app/src/todo.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import "./style.css";
+
+export const Todo = ()=> {
+
+
+  const [todoText,setTodoText] = useState("");
+  const [incomplateTodos,setIncomplateTodos] = useState([
+    "todoです",
+    "todoです2",
+  ]);
+
+
+  const [complateTodos,setComplateTodos] = useState([
+                       "todoでした",
+                       "todoでした2"
+                       ]);
+
+
+  const onChangeTodoText =  (event) => setTodoText(event.target.value);
+
+// onchangeなどのイベントが発火すると、「eventという引数が渡ってくるようになっている。」
+
+  const onClickAdd = () => {
+    if (todoText === "")return;
+    const newTodos = [...incomplateTodos,todoText];
+    setIncomplateTodos(newTodos);
+    setTodoText("");
+  };
+
+  return (
+
+     <>
+
+      <div className="input-area">
+        <input placeholder="Todoを入力" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
+
+      </div>
+
+{/* onchangeってなんだ？ 　＝>  テキストボックスに変更があった時に発火するイベント*/}
+
+
+
+      <div className="incomplate-area">
+        <p className="title">未完了のtodo</p>
+        <ul>
+          {incomplateTodos.map((todo) => (
+              <li key ={todo} >
+                  <div className="list-row">
+                    <p className="todo-item">{todo}</p>
+                    <button>完了</button>
+                    <button>削除</button>
+                  </div>
+              </li>
+
+
+
+            )
+
+          ) }
+
+        </ul>
+      </div>
+
+
+
+
+
+      <div className = "complate-area">
+        <p className="title">完了のtodo</p>
+        <ul>
+            {complateTodos.map((Todo) => (
+
+              <li key ={Todo} >
+                    <div className="list-row">
+                      <p className="todo-item">{Todo}</p>
+                      <button>戻す</button>
+                    </div>
+              </li>
+                 )
+                )
+            }
+
+
+        </ul>
+      </div>
+
+
+
+    </>
+
+
+
+  );
+};
